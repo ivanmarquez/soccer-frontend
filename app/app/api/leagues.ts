c8234654dae6be7b4863e173ce64dc59172fa6f6
Ivan Marquez <ivanjesmh@gmail.com>
2024-12-05 23:25:27 -0600

    debug

--- app/app/api/leagues.ts
+++ app/app/api/leagues.ts
@@ -8,6 +8,7 @@ export interface League {
 
 
 export const apiFetchLeagues = async (): Promise<League[]> => {
+    console.log(process.env.NEXT_PUBLIC_API_URL);
     const response: { data: League[] } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/leagues`);
     return response.data;
 };
