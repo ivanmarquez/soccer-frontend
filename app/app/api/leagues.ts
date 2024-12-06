e3568b56a914218da38ad5f6568bab28eeec414f
Ivan Marquez <ivanjesmh@gmail.com>
2024-12-05 23:56:44 -0600

    return to original

--- app/app/api/leagues.ts
+++ app/app/api/leagues.ts
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 
 export interface League {
@@ -7,16 +8,6 @@ export interface League {
 
 
 export const apiFetchLeagues = async (): Promise<League[]> => {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
-
-    if (!process.env.NEXT_PUBLIC_API_URL) {
-        throw new Error("API URL is not defined");
-    }
-
-    const apiUrl = new URL(process.env.NEXT_PUBLIC_API_URL);
-    apiUrl.protocol = 'https:';
-
-    //CUSTOM HARDCODED URL
-    const response: { data: League[] } = await axios.get(`https://soccer-fastapi.onrender.com/leagues`);
+    const response: { data: League[] } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/leagues`);
     return response.data;
 };
